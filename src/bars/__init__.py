7353af6799e3b8f7e04f08e669a42f76e7640978
Huberkeep <anithce@outlook.com>
2026-01-31 17:46:19 +0800

    Implement user input for file selection
    Add user input prompt for file selection

--- src/bars/__init__.py
+++ src/bars/__init__.py
@@ -1,3 +1,8 @@
 from .firatbars import ceicfirmbar,icrinputbar
 __all__ = ["ceicfirmbar","icrinputbar"]
-  
+userInput = input("""Welcome!
+list:l
+more:m""")
+if UserInput == l:
+  print("file firatbars.py")
+
